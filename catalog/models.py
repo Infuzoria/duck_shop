3130040e748cc6bc1dc17960214046f636bf1dac
shurochka <alexa.sazhaeva@gmail.com>
2023-10-03 03:42:01 +0300

    Заполнили модель Product, создали миграцию

--- catalog/models.py
+++ catalog/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=800, verbose_name='Описание')
+    image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Фото')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    date_of_creation = models.DateField(**NULLABLE, verbose_name='Дата создания')
+    last_modified_date = models.DateField(**NULLABLE, verbose_name='Дата последнего изменения')
 
     def __str__(self):
-        pass
+        return f'{self.name} ({self.category})'
 
     class Meta:
-        pass
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
 
 
 class Category(models.Model):
