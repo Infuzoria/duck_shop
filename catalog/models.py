cd95f3f9a8aaf01f5df44f6febf526af97600073
shurochka <alexa.sazhaeva@gmail.com>
2023-10-03 03:50:03 +0300

    Наполнили модель Category

--- catalog/models.py
+++ catalog/models.py
@@ -3,11 +3,23 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=800, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.CharField(max_length=800, verbose_name='Описание')
     image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Фото')
-    category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
     date_of_creation = models.DateField(**NULLABLE, verbose_name='Дата создания')
     last_modified_date = models.DateField(**NULLABLE, verbose_name='Дата последнего изменения')
@@ -18,12 +30,3 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
-
-
-class Category(models.Model):
-
-    def __str__(self):
-        pass
-
-    class Meta:
-        pass
