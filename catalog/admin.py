62c8c08a8424a383447823e8377d13b76f08b59f
shurochka <alexa.sazhaeva@gmail.com>
2023-10-03 05:02:20 +0300

    Задали отображение для категорий

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
