401fbcddaa90890baaa119ba9c1174e8fe63bf41
shurochka <alexa.sazhaeva@gmail.com>
2023-10-29 15:00:31 +0300

    Внесли небольшие коррективы

--- catalog/models.py
+++ catalog/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     description = models.CharField(max_length=800, verbose_name='Описание')
 
     def __str__(self):
-        return f'{self.name} ({self.description})'
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'категория'
