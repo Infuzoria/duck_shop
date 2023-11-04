141fb2af7a92b211c25f927fc9c9405afefda2e0
shurochka <alexa.sazhaeva@gmail.com>
2023-11-04 22:25:55 +0300

    Реализовали контроллер для редактирования клиентов

--- mailing_app/urls.py
+++ mailing_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from mailing_app.views import ClientCreateView, ClientListView
+from mailing_app.views import ClientCreateView, ClientListView, ClientUpdateView
 
 app_name = 'mailing_app'
 
 urlpatterns = [
-    #path('mailing/', index, name='base'),
     path('create_client/', ClientCreateView.as_view(), name='create_client'),
     path('clients/', ClientListView.as_view(), name="list_of_clients"),
+    path('client_update/<int:pk>/', ClientUpdateView.as_view(), name='update_client'),
 ]
--- mailing_app/views.py
+++ mailing_app/views.py
@@ -1,6 +1,6 @@
 from mailing_app.forms import ClientForm
 from mailing_app.models import Client
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 
 
 class ClientCreateView(CreateView):
@@ -11,3 +11,9 @@ class ClientCreateView(CreateView):
 
 class ClientListView(ListView):
     model = Client
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = '/clients'
