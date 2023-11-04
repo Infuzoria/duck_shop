b44035ae5b7f4369d2ad9e0fe6682bdac8b95de0
shurochka <alexa.sazhaeva@gmail.com>
2023-11-04 19:42:53 +0300

    Создали сущность Newsletter

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -12,3 +12,16 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'клиент'
         verbose_name_plural = 'клиенты'
+
+
+class Newsletter(models.Model):
+    time = models.DateField(verbose_name='Время рассылки')
+    period = models.CharField(max_length=100, verbose_name=' Периодичность рассылки')
+    status = models.CharField(default='created', max_length=20, verbose_name='Статус рассылки')
+
+    def __str__(self):
+        return f'{self.time} {self.period} {self.status}'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
