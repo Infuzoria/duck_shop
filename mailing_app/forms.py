8fe9b66078b0931c98d93504b0cacbb868560b3e
shurochka <alexa.sazhaeva@gmail.com>
2023-11-04 21:37:52 +0300

    Создали форму для сущности Text

--- mailing_app/forms.py
+++ mailing_app/forms.py
@@ -1,6 +1,9 @@
 import re
 from django import forms
-from mailing_app.models import Client
+from mailing_app.models import Client, Text
+
+PROHIBITED_CATEGORIES = ['казино', 'криптовалюта', 'крипта', 'баржа', 'дешево', 'бесплатно', 'обман',
+                         'полиция', 'радар']
 
 
 class ClientForm(forms.ModelForm):
@@ -16,3 +19,27 @@ class ClientForm(forms.ModelForm):
             return cleaned_data
         else:
             raise forms.ValidationError('Адрес почты введен некорректно')
+
+
+class TextForm(forms.ModelForm):
+    class Meta:
+        model = Text
+        fields = '__all__'
+
+    def clean_topic(self):
+        cleaned_data = self.cleaned_data.get('topic')
+
+        if all(word not in cleaned_data.lower() for word in PROHIBITED_CATEGORIES):
+            return cleaned_data
+
+        else:
+            raise forms.ValidationError('В теме присутствует упоминание запрещенной категории')
+
+    def clean_text(self):
+        cleaned_data = self.cleaned_data.get('text')
+
+        if all(word not in cleaned_data.lower() for word in PROHIBITED_CATEGORIES):
+            return cleaned_data
+
+        else:
+            raise forms.ValidationError('В сообщении присутствует упоминание запрещенной категории')
