a7338d119485d812cc90eddfd00d56a18d3b7a80
shurochka <alexa.sazhaeva@gmail.com>
2023-11-04 21:32:18 +0300

    Добавили поле message в сущность Newsletter

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Client(models.Model):
     email = models.CharField(max_length=100, verbose_name='Email пользователя')
@@ -14,19 +16,6 @@ class Client(models.Model):
         verbose_name_plural = 'клиенты'
 
 
-class Newsletter(models.Model):
-    time = models.DateField(verbose_name='Время рассылки')
-    period = models.CharField(max_length=100, verbose_name=' Периодичность рассылки')
-    status = models.CharField(default='created', max_length=20, verbose_name='Статус рассылки')
-
-    def __str__(self):
-        return f'{self.time} {self.period} {self.status}'
-
-    class Meta:
-        verbose_name = 'рассылка'
-        verbose_name_plural = 'рассылки'
-
-
 class Text(models.Model):
     topic = models.CharField(max_length=100, verbose_name='Тема письма')
     text = models.TextField(verbose_name='Тело письма')
@@ -37,6 +26,20 @@ class Text(models.Model):
     class Meta:
         verbose_name = 'сообщение'
         verbose_name_plural = 'сообщения'
+
+
+class Newsletter(models.Model):
+    time = models.DateField(verbose_name='Время рассылки')
+    period = models.CharField(max_length=100, verbose_name=' Периодичность рассылки')
+    status = models.CharField(default='created', max_length=20, verbose_name='Статус рассылки')
+    message = models.ForeignKey(Text, on_delete=models.CASCADE, **NULLABLE, verbose_name='Текст рассылки')
+
+    def __str__(self):
+        return f'{self.time} {self.period} {self.status}'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
 
 
 class Logs(models.Model):
