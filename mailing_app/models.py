757e36010d40080ad759a42ac4a9bac6d6f05990
shurochka <alexa.sazhaeva@gmail.com>
2023-11-04 19:42:06 +0300

    Создали сущность Client

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    email = models.CharField(max_length=100, verbose_name='Email пользователя')
+    name = models.CharField(max_length=100, verbose_name='ФИО пользователя')
+    text = models.TextField(verbose_name='Комментарий')
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
