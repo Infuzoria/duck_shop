3cd6c03d30023c07a922f06d5d42ba147d2074bc
shurochka <alexa.sazhaeva@gmail.com>
2023-11-09 22:55:18 +0300

    Исправили модели, добавили недостающие поля, сделали новую миграцию

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -29,13 +29,28 @@ class Text(models.Model):
 
 
 class Newsletter(models.Model):
-    time = models.DateField(verbose_name='Время рассылки')
-    period = models.CharField(max_length=100, verbose_name=' Периодичность рассылки')
-    status = models.CharField(default='created', max_length=20, verbose_name='Статус рассылки')
-    message = models.ForeignKey(Text, on_delete=models.CASCADE, **NULLABLE, verbose_name='Текст рассылки')
+
+    PERIODICITY = (
+        ('day', 'Один раз в день'),
+        ('week', 'Один раз в неделю'),
+        ('month', 'Один раз в месяц'),
+    )
+
+    STATUS = (
+        ('completed', 'Завершена'),
+        ('created', 'Создана'),
+        ('launched', 'Запущена'),
+    )
+
+    start_time = models.DateField(verbose_name='Время начала рассылки')
+    stop_time = models.DateField(verbose_name='Время окончания рассылки')
+    period = models.CharField(max_length=100, choices=PERIODICITY, verbose_name=' Периодичность рассылки')
+    status = models.CharField(default='created', max_length=20, choices=STATUS, verbose_name='Статус рассылки')
+    message = models.ForeignKey(Text, on_delete=models.CASCADE, verbose_name='Текст рассылки')
+    client = models.ManyToManyField(Client)
 
     def __str__(self):
-        return f'{self.time} {self.period} {self.status}'
+        return f'{self.start_time} {self.period} {self.status}'
 
     class Meta:
         verbose_name = 'рассылка'
@@ -44,11 +59,13 @@ class Newsletter(models.Model):
 
 class Logs(models.Model):
     date = models.DateField(verbose_name='Дата и время последней попытки')
-    status = models.CharField(max_length=50, verbose_name='Статус попытки')
-    reply = models.CharField(max_length=300, verbose_name='Ответ почтового сервера')
+    status = models.BooleanField(verbose_name='Статус рассылки')
+    error_msg = models.CharField(default='None', max_length=300, verbose_name='Ответ почтового сервера')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='Пользователь')
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE, verbose_name='Рассылка')
 
     def __str__(self):
-        return f'{self.date} {self.status} ({self.reply})'
+        return f'{self.date} {self.status} ({self.error_msg})'
 
     class Meta:
         verbose_name = 'лог'
