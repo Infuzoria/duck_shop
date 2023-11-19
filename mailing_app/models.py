5ee5388eea54101eb7d886284478c5dc4db01917
shurochka <alexa.sazhaeva@gmail.com>
2023-11-19 11:04:09 +0300

    Добавили в модель client поле owner_id

--- mailing_app/models.py
+++ mailing_app/models.py
@@ -8,6 +8,7 @@ class Client(models.Model):
     email = models.CharField(max_length=100, verbose_name='Email пользователя')
     name = models.CharField(max_length=100, verbose_name='ФИО пользователя')
     text = models.TextField(verbose_name='Комментарий')
+    owner_id = models.ForeignKey(User, **NULLABLE, on_delete=models.CASCADE, verbose_name='id создателя')
 
     def __str__(self):
         return f'{self.name} ({self.email})'
