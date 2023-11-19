426ee64ed905d051f91cc76e7a930e90d4087a30
shurochka <alexa.sazhaeva@gmail.com>
2023-11-19 11:24:28 +0300

    Реализовали фильтрацию клиентов, чтобы пользователи могли видеть только своих клиентов

--- mailing_app/views.py
+++ mailing_app/views.py
@@ -18,6 +18,11 @@ class ClientListView(PermissionRequiredMixin, ListView):
     permission_required = 'mailing_app.view_client'
     model = Client
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(owner_id=self.request.user.id)
+        return queryset
+
 
 class ClientUpdateView(PermissionRequiredMixin, UpdateView):
     permission_required = 'mailing_app.change_client'
